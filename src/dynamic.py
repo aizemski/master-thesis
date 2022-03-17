de5310bf41113c2f543135ab547d77d5dbaafd4d
aizemski <aleksanderizemski@gmail.com>
2022-03-18 00:00:08 +0100

    Created function which calculate value function. Prepared file structure for future development

--- src/dynamic.py
+++ src/dynamic.py
@@ -2,28 +2,24 @@ import pandas as pd
 import numpy as np
 
 
-def mi(time, state, portfolio):
-    """
-    state:
-        0 - cash
-        1 - asset
-    """
-    cash  = mi(time-1, 0, portfolio) portfolio.asset,
-    return max()
-    pass
+def action(state, current, next, fee=0.0):
+    fee = np.log(1 - fee)
+    if len(next) > 1:
+        current = current * int(bool(state)) + max(
+            (action(state=1, current=next.iloc[1], next=next.iloc[1:], fee=fee) - (fee * int(bool(state)))),
+            (action(state=0, current=next.iloc[1], next=next.iloc[1:], fee=fee) + (fee * (1 - int(bool(state)))))
+        )
+    else:
+        current = (current - fee) * int(bool(state))
+    return current
 
 
-def dynamic(data, cash):
-    """
-        actions:
-            1 - buy
-            0 - sell
-    """
-    portfolio = {'cash': cash, 'asset': 0}
-    actions = []
+def optimal(state, frame, prediction_period=1, fee=0.0):
+    frame = np.log(frame[1:])
 
-
-"""
-s^ = s - b
-f^_s = log(1+f_s) - transaction cost?
-"""
+    while len(state) < len(frame):
+        state.append(
+            [action(state=0, current=frame.iloc[len(state) - 1],
+                    next=frame[len(state) - 1:min(len(state) + prediction_period, len(frame) - 1)], fee=fee),
+             action(state=1, current=frame.iloc[len(state) - 1],
+                    next=frame[len(state) - 1:min(len(state) + prediction_period, len(frame) - 1)], fee=fee)])
--- src/main.py
+++ src/main.py
@@ -1,22 +1,37 @@
 from constants import *
 from download import download_monthly_klines as download
 import pandas as pd
+import numpy as np
 import os
 import glob
+
+from src.dynamic import optimal
 
 if __name__ == '__main__':
     i = 0
     # download(trading_type=TRADING_TYPE[0], symbols=[TICKERS[i] +
-    #          BASE],intervals=['15m'], years=[2020, 2021], months=list(range(1, 13)))
+    #                                                 BASE], intervals=['15m'], years=[2020, 2022],
+    #          months=list(range(1, 13)))
     all_files = glob.glob(f'../data/{TICKERS[i] + BASE}/15m/*.csv')
-    # print(all_files)
-    # pd.read_csv(f'../data/{TICKERS[i] + BASE}/15m')
+
     li = []
-
     for filename in all_files:
-        df = pd.read_csv(filename, index_col=None, header=1)
+        df = pd.read_csv(filename, index_col=None,
+                         names=["Open time", "Open", "High", "Low", "Close", "Volume", "Close time",
+                                "Quote asset volume", "Number of trades", "Taker buy base asset volume",
+                                "Taker buy quote asset volume", "Ignore"])
         li.append(df)
-        break
 
-    frame = pd.concat(li, axis=0, ignore_index=True)
-    print(frame)
+    frame = pd.concat(li, axis=0, ignore_index=False)
+    frame['Open time'] = pd.to_datetime(frame['Open time'], unit='ms')
+    frame.set_index(keys='Open time', inplace=True)
+    frame = frame.sort_index(ascending=True)
+    frame_dynamic = frame[['Open', 'Close']]
+
+    state = []
+    portfolio = np.log(1)
+
+    state.append([portfolio, portfolio])
+    optimal(state, (frame_dynamic.iloc[:]['Close'].pct_change().fillna(0) + 1), prediction_period=5)
+    print(len(state))
+    print(len(frame))
