95080689cb99c97b40a1f973b64d81c8c84d8939
aizemski <aleksanderizemski@gmail.com>
2022-03-26 01:45:47 +0100

    Changing value function to best choice for time period

--- src/dynamic.py
+++ src/dynamic.py
@@ -1,25 +1,40 @@
-import pandas as pd
+import time
+
 import numpy as np
+import pandas as pd
 
 
-def action(state, current, next, fee=0.0):
-    fee = np.log(1 - fee)
-    if len(next) > 1:
-        current = current * int(bool(state)) + max(
-            (action(state=1, current=next.iloc[1], next=next.iloc[1:], fee=fee) - (fee * int(bool(state)))),
-            (action(state=0, current=next.iloc[1], next=next.iloc[1:], fee=fee) + (fee * (1 - int(bool(state)))))
+def action_for_all_data(state, current_value, next_value, result_array, visited, fee=0.0):
+    log_fee = np.log(1 - fee)
+    if len(next_value) > 1:
+        current_value = current_value * int(bool(state)) + max(
+            (action_for_all_data(state=1, current_value=next_value.iloc[1], next_value=next_value.iloc[1:],
+                                 result_array=result_array, visited=visited,
+                                 fee=fee) - (
+                     log_fee * int(bool(state))))
+            if visited[1, -len(next_value)] == 0
+            else result_array[1, -len(next_value)],
+            (action_for_all_data(state=0, current_value=next_value.iloc[1], next_value=next_value.iloc[1:],
+                                 result_array=result_array, visited=visited,
+                                 fee=fee) + (
+                     log_fee * (1 - int(bool(state)))))
+            if visited[0, -len(next_value)] == 0
+            else result_array[0, -len(next_value)],
         )
     else:
-        current = (current - fee) * int(bool(state))
-    return current
+        current_value = (current_value - log_fee) * int(bool(state))
+    result_array[state, -len(next_value)] = current_value
+    visited[state, -len(next_value)] = 1
+    return current_value
 
 
-def optimal(state, frame, prediction_period=1, fee=0.0):
-    frame = np.log(frame[1:])
-
-    while len(state) < len(frame):
-        state.append(
-            [action(state=0, current=frame.iloc[len(state) - 1],
-                    next=frame[len(state) - 1:min(len(state) + prediction_period, len(frame) - 1)], fee=fee),
-             action(state=1, current=frame.iloc[len(state) - 1],
-                    next=frame[len(state) - 1:min(len(state) + prediction_period, len(frame) - 1)], fee=fee)])
+def optimal_for_all_data(frame, fee=0.0):
+    start_time = time.time()
+    frame_close_change = ((frame['Close']).pct_change().fillna(0) + 1)
+    frame_close_change = np.log(frame_close_change)
+    result_array = np.zeros((2, len(frame_close_change)))
+    visited = np.zeros((2, len(frame_close_change)))
+    action_for_all_data(state=0, current_value=frame_close_change.iloc[0],
+                        next_value=frame_close_change[1:], result_array=result_array, visited=visited, fee=fee)
+    print(time.time() - start_time)
+    return pd.DataFrame(result_array[0]), pd.DataFrame(result_array[1])
--- src/main.py
+++ src/main.py
@@ -5,33 +5,14 @@ import numpy as np
 import os
 import glob
 
-from src.dynamic import optimal
+from src.data import load_all_downloaded_data_for_pair, prepare_data_for_close
+from src.dynamic import optimal_for_all_data
 
 if __name__ == '__main__':
     i = 0
     # download(trading_type=TRADING_TYPE[0], symbols=[TICKERS[i] +
     #                                                 BASE], intervals=['15m'], years=[2020, 2022],
     #          months=list(range(1, 13)))
-    all_files = glob.glob(f'../data/{TICKERS[i] + BASE}/15m/*.csv')
-
-    li = []
-    for filename in all_files:
-        df = pd.read_csv(filename, index_col=None,
-                         names=["Open time", "Open", "High", "Low", "Close", "Volume", "Close time",
-                                "Quote asset volume", "Number of trades", "Taker buy base asset volume",
-                                "Taker buy quote asset volume", "Ignore"])
-        li.append(df)
-
-    frame = pd.concat(li, axis=0, ignore_index=False)
-    frame['Open time'] = pd.to_datetime(frame['Open time'], unit='ms')
-    frame.set_index(keys='Open time', inplace=True)
-    frame = frame.sort_index(ascending=True)
-    frame_dynamic = frame[['Open', 'Close']]
-
-    state = []
-    portfolio = np.log(1)
-
-    state.append([portfolio, portfolio])
-    optimal(state, (frame_dynamic.iloc[:]['Close'].pct_change().fillna(0) + 1), prediction_period=5)
-    print(len(state))
-    print(len(frame))
+    frame = load_all_downloaded_data_for_pair(TICKERS[i], BASE)
+    frame = frame[-300:]
+    frame['Cash'], frame['Asset'] = optimal_for_all_data(frame)
